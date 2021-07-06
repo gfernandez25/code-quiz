e4b51587e2079ab7b1071275941a008ee0aa2090
gfernandez25 <82284313+gfernandez25@users.noreply.github.com>
2021-07-06 00:46:47 -0400

    timer countdown functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,40 @@
-//create variable to target timer element
-var displayTimer = document.getElementById("display-timer")
+// variable to store initial time
+var timer = 100;
 
-console.log(timer); // testing target
+//create variable to target div timer element
+var displayTimer = document.getElementById("display-timer")
 
 //create variable to target start button
 var buttonStart = document.getElementById("button-start-quiz");
 
-console.log(buttonStart); //testing proper target
 
 //add event listener to button to start the quiz
-buttonStart.addEventListener("click", function (){
-    window.alert("start button clicked") //testing event listener
+buttonStart.addEventListener("click", function () {
+        window.alert("start button clicked") //testing event listener
+        currenTimer = startCountdown(); //did it this way to be able so use clearinterval
     }
-
 )
 
-//create timer
-//create variable to hold time
-var timer = 100;
+
+
+//function that will run every second  and call function that reduce total time remaining, return time remaining
+function startCountdown() {
+    var currenTimer = setInterval(countDown, 1000);
+    return currenTimer;
+}
 
 //function that will reduce total time remaining by one and display on the html using DOM
-function countDown(){
-    displayCount()
+function countDown() {
+    displayCount(timer)
     console.log(timer); //change this to display in page
     timer--;
-    if (timer === 0){
-    clearInterval(currenTimer)
+    if (timer === -1) {
+        clearInterval(currenTimer) //use this to stop set interval after 0
     }
 }
 
-//function that will run every second  and call function that reduce total time remaining
-var currenTimer = setInterval(countDown, 1000)
-
-
 //function that displays the current count on the page by using DOM targeting
-function displayCount(){
+function displayCount(timer) {
     displayTimer.textContent = timer;
 }
 
@@ -42,13 +42,13 @@ function displayCount(){
 //create questions
 
 //logic for score
-    //ask questions and provide possible answers
-    //user selects possible answer
-    //answer gets evaluated right or wrong
-    //if right move to next question ----- if wrong move to next question and subtract 10 seconds from timer
+//ask questions and provide possible answers
+//user selects possible answer
+//answer gets evaluated right or wrong
+//if right move to next question ----- if wrong move to next question and subtract 10 seconds from timer
 
 //end of game
-    //all questions answered or timer reaches zero
-    //user is presented with option to enter initials and submit
-    //list of high scores is displayed
-    //provide ability to clear all scores or go back to start
+//all questions answered or timer reaches zero
+//user is presented with option to enter initials and submit
+//list of high scores is displayed
+//provide ability to clear all scores or go back to start
